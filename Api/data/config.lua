ddc9161f0fa58aa491c006a135fe953bb8bf6eb0
RaDiCaLbOtTeAm <33381626+RaDiCaLbOtTeAm@users.noreply.github.com>
2018-02-20 22:56:54 +0330

    Create config.lua

---
+++ Api/data/config.lua
@@ -0,0 +1,19 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "Helper",
+    "plugins"
+  },
+  info_text = "💥MeGa Plus Bot \n\n▪️Version:\n(TDBot v3.0)\n\n▫️GitHub™\n\nhttps://github.com\n\n》Admins\n》@KingLonely and @SajjadMazini ➣ Founder & Developer\n》@Mohammadzd ➣ Developer\n\n💥Our channel :\n》@MeGaPlusTeaM《\n",
+  moderation = {
+    data = "/home/megaplus1/MeGaPlus/Cli/data/moderation.json"
+  },
+  sudo_users = {
+    482089569,
+    488565793,
+    516163430
+  }
+}
+return _
+end
